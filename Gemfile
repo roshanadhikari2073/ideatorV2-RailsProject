93319b77fde15953e30d48a40254f27f43d76872
roshanadhikari2073 <greenboyroshan@gmail.com>
2016-12-01 21:56:23 +0545

    new

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'simple_form'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,17 @@
+
+
 class IdeasController < ApplicationController
   def index
-  @ideas = Idea.all
+    @ideas = Idea.all
+  end
+
+  def create
+    @idea = Idea.create(idea_params)
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:description, :author)
   end
 end
