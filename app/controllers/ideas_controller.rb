ca09979bc904a185c97cf27e7d38333379f0921a
roshanadhikari2073 <greenboyroshan@gmail.com>
2016-12-03 10:59:28 +0545

    delete

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,8 +7,21 @@ class IdeasController < ApplicationController
 
   def create
     @idea = Idea.create(idea_params)
+    redirect_to root_path
   end
-
+  def edit
+    @idea = Idea.find(params[:id])
+    if @idea.update(idea_params)
+    redirect_to root_path
+  else
+    redirect_to edit_idea_path(params[:id])
+  end
+  end
+  def destroy
+  @idea = Idea.find(params[:id])
+  @idea.destroy
+  redirect_to root_path
+end
   private
 
   def idea_params
